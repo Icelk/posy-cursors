86a46cd9373957c85effe1ae83e68641f7752ff2
Dmitry <dmitry@auteiy.me>
2019-10-25 21:16:02 +0400

    Add black theme

--- build.sh
+++ build.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# Capitaine cursors, macOS inspired cursors based on KDE Breeze
-# Copyright (c) 2016 Keefer Rourke <keefer.rourke@gmail.com>
-# Modified to include white cursors by Efus10n - 4 Mar 2019
-
-function create {
-	cd "$SRC"
+function buildWhite {
+	cd "$sourceWhite"
 	mkdir -p x1 x1_25 x1_5 x2
 	# generate cursors
-	if [[ "$THEME" =~ White$ ]]; then
-		BUILD="$SRC"/../dist-white
-	else BUILD="$SRC"/../posy-default
-	fi
+	BUILD="$sourceWhite"/../posy-white
 	OUTPUT="$BUILD"/cursors
-	ALIASES="$SRC"/cursorList
+	ALIASES="$sourceWhite"/cursorList
 
 	if [ ! -d "$BUILD" ]; then
 		mkdir "$BUILD"
@@ -53,17 +46,67 @@ function create {
 	INDEX="$OUTPUT/../index.theme"
 	if [ ! -e "$OUTPUT/../$INDEX" ]; then
 		touch "$INDEX"
-		echo -e "[Icon Theme]\nName=$THEME\n" > "$INDEX"
+		echo -e "[Icon Theme]\nName=$themeWhite\n" > "$INDEX"
 	fi
 	echo -e "Generating Theme Index... DONE"
 }
 
-# generate pixmaps from svg source
-SRC=$PWD/src
-THEME="Posy Default"
+function buildBlack {
+	cd "$sourceBlack"
+	mkdir -p x1 x1_25 x1_5 x2
+	# generate cursors
+	BUILD="$sourceBlack"/../posy-black
+	OUTPUT="$BUILD"/cursors
+	ALIASES="$sourceBlack"/cursorList
 
-create svg
+	if [ ! -d "$BUILD" ]; then
+		mkdir "$BUILD"
+	fi
+	if [ ! -d "$OUTPUT" ]; then
+		mkdir "$OUTPUT"
+	fi
 
-# THEME="Capitaine Cursors - White"
+	echo -ne "Generating cursor theme...\\r"
+	for CUR in config/*.cursor; do
+		BASENAME="$CUR"
+		BASENAME="${BASENAME##*/}"
+		BASENAME="${BASENAME%.*}"
+		
+		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
+	done
+	echo -e "Generating cursor theme... DONE"
 
-# create svg-white
+	cd "$OUTPUT"	
+
+	#generate aliases
+	echo -ne "Generating shortcuts...\\r"
+	while read ALIAS; do
+		FROM="${ALIAS#* }"
+		TO="${ALIAS% *}"
+
+		if [ -e $TO ]; then
+			continue
+		fi
+		ln -sr "$FROM" "$TO"
+	done < "$ALIASES"
+	echo -e "Generating shortcuts... DONE"
+
+	cd "$PWD"
+
+	echo -ne "Generating Theme Index...\\r"
+	INDEX="$OUTPUT/../index.theme"
+	if [ ! -e "$OUTPUT/../$INDEX" ]; then
+		touch "$INDEX"
+		echo -e "[Icon Theme]\nName=$themeBlack\n" > "$INDEX"
+	fi
+	echo -e "Generating Theme Index... DONE"
+}
+
+sourceWhite=$PWD/White
+themeWhite="Posy White"
+
+sourceBlack=$PWD/Black
+themeBlack="Posy Black"
+
+buildWhite
+buildBlack
