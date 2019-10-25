485d4b49a4b03c06a602f2012a79a23700213d54
Dmitry <dmitry@auteiy.me>
2019-10-25 23:03:01 +0400

    Add themes with tiny cursor

--- build.sh
+++ build.sh
@@ -102,11 +102,121 @@ function buildBlack {
 	echo -e "Generating Theme Index... DONE"
 }
 
+function buildBlackTiny {
+	cd "$sourceBlackTiny"
+	mkdir -p x1 x1_25 x1_5 x2
+	# generate cursors
+	BUILD="$sourceBlackTiny"/../posy-black-tiny
+	OUTPUT="$BUILD"/cursors
+	ALIASES="$sourceBlack"/cursorList
+
+	if [ ! -d "$BUILD" ]; then
+		mkdir "$BUILD"
+	fi
+	if [ ! -d "$OUTPUT" ]; then
+		mkdir "$OUTPUT"
+	fi
+
+	echo -ne "Generating cursor theme...\\r"
+	for CUR in config/*.cursor; do
+		BASENAME="$CUR"
+		BASENAME="${BASENAME##*/}"
+		BASENAME="${BASENAME%.*}"
+		
+		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
+	done
+	echo -e "Generating cursor theme... DONE"
+
+	cd "$OUTPUT"	
+
+	#generate aliases
+	echo -ne "Generating shortcuts...\\r"
+	while read ALIAS; do
+		FROM="${ALIAS#* }"
+		TO="${ALIAS% *}"
+
+		if [ -e $TO ]; then
+			continue
+		fi
+		ln -sr "$FROM" "$TO"
+	done < "$ALIASES"
+	echo -e "Generating shortcuts... DONE"
+
+	cd "$PWD"
+
+	echo -ne "Generating Theme Index...\\r"
+	INDEX="$OUTPUT/../index.theme"
+	if [ ! -e "$OUTPUT/../$INDEX" ]; then
+		touch "$INDEX"
+		echo -e "[Icon Theme]\nName=$themeBlackTiny\n" > "$INDEX"
+	fi
+	echo -e "Generating Theme Index... DONE"
+}
+
+function buildWhiteTiny {
+	cd "$sourceWhiteTiny"
+	mkdir -p x1 x1_25 x1_5 x2
+	# generate cursors
+	BUILD="$sourceWhiteTiny"/../posy-white-tiny
+	OUTPUT="$BUILD"/cursors
+	ALIASES="$sourceWhiteTiny"/cursorList
+
+	if [ ! -d "$BUILD" ]; then
+		mkdir "$BUILD"
+	fi
+	if [ ! -d "$OUTPUT" ]; then
+		mkdir "$OUTPUT"
+	fi
+
+	echo -ne "Generating cursor theme...\\r"
+	for CUR in config/*.cursor; do
+		BASENAME="$CUR"
+		BASENAME="${BASENAME##*/}"
+		BASENAME="${BASENAME%.*}"
+		
+		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
+	done
+	echo -e "Generating cursor theme... DONE"
+
+	cd "$OUTPUT"	
+
+	#generate aliases
+	echo -ne "Generating shortcuts...\\r"
+	while read ALIAS; do
+		FROM="${ALIAS#* }"
+		TO="${ALIAS% *}"
+
+		if [ -e $TO ]; then
+			continue
+		fi
+		ln -sr "$FROM" "$TO"
+	done < "$ALIASES"
+	echo -e "Generating shortcuts... DONE"
+
+	cd "$PWD"
+
+	echo -ne "Generating Theme Index...\\r"
+	INDEX="$OUTPUT/../index.theme"
+	if [ ! -e "$OUTPUT/../$INDEX" ]; then
+		touch "$INDEX"
+		echo -e "[Icon Theme]\nName=$themeWhiteTiny\n" > "$INDEX"
+	fi
+	echo -e "Generating Theme Index... DONE"
+}
+
 sourceWhite=$PWD/White
 themeWhite="Posy White"
 
 sourceBlack=$PWD/Black
 themeBlack="Posy Black"
 
+sourceWhiteTiny=$PWD/WhiteTiny
+themeWhiteTiny="Posy White Tiny"
+
+sourceBlackTiny=$PWD/BlackTiny
+themeBlackTiny="Posy Black Tiny"
+
 buildWhite
 buildBlack
+buildWhiteTiny
+buildBlackTiny
